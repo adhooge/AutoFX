2d78775e5e1cd54b91c6e11421a63056fe4c7abe
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-04-04 16:38:36 +0200

    [+]: Confusion matrix function

--- source/util.py
+++ source/util.py
@@ -10,12 +10,112 @@ from pedalboard.io import AudioFile
 import soundfile as sf
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from scipy import signal
 from numpy.typing import ArrayLike
 
 NUM_COEFF_CEPSTRUM = 10
 GUITAR_MIN_FREQUENCY = 80
 GUITAR_MAX_FREQUENCY = 1200
+CLASSES = ['Dry', 'Feedback Delay', 'Slapback Delay', 'Reverb', 'Chorus', 'Flanger', 'Phaser',
+           'Tremolo', 'Vibrato', 'Distortion', 'Overdrive']
+
+
+def make_confusion_matrix(cf,
+                          group_names=None,
+                          categories='auto',
+                          count=True,
+                          percent=True,
+                          cbar=True,
+                          xyticks=True,
+                          xyplotlabels=True,
+                          sum_stats=True,
+                          figsize=None,
+                          cmap='Blues',
+                          title=None):
+    '''
+    This function will make a pretty plot of an sklearn Confusion Matrix cm using a Seaborn heatmap visualization.
+    Arguments
+    ---------
+    cf:            confusion matrix to be passed in
+    group_names:   List of strings that represent the labels row by row to be shown in each square.
+    categories:    List of strings containing the categories to be displayed on the x,y axis. Default is 'auto'
+    count:         If True, show the raw number in the confusion matrix. Default is True.
+    normalize:     If True, show the proportions for each category. Default is True.
+    cbar:          If True, show the color bar. The cbar values are based off the values in the confusion matrix.
+                   Default is True.
+    xyticks:       If True, show x and y ticks. Default is True.
+    xyplotlabels:  If True, show 'True Label' and 'Predicted Label' on the figure. Default is True.
+    sum_stats:     If True, display summary statistics below the figure. Default is True.
+    figsize:       Tuple representing the figure size. Default will be the matplotlib rcParams value.
+    cmap:          Colormap of the values displayed from matplotlib.pyplot.cm. Default is 'Blues'
+                   See http://matplotlib.org/examples/color/colormaps_reference.html
+
+    title:         Title for the heatmap. Default is None.
+    '''
+
+    # CODE TO GENERATE TEXT INSIDE EACH SQUARE
+    blanks = ['' for i in range(cf.size)]
+
+    if group_names and len(group_names) == cf.size:
+        group_labels = ["{}\n".format(value) for value in group_names]
+    else:
+        group_labels = blanks
+
+    if count:
+        group_counts = ["{0:0.0f}\n".format(value) for value in cf.flatten()]
+    else:
+        group_counts = blanks
+
+    if percent:
+        group_percentages = ["{0:.2%}".format(value) for value in cf.flatten() / np.sum(cf)]
+    else:
+        group_percentages = blanks
+
+    box_labels = [f"{v1}{v2}{v3}".strip() for v1, v2, v3 in zip(group_labels, group_counts, group_percentages)]
+    box_labels = np.asarray(box_labels).reshape(cf.shape[0], cf.shape[1])
+
+    # CODE TO GENERATE SUMMARY STATISTICS & TEXT FOR SUMMARY STATS
+    if sum_stats:
+        # Accuracy is sum of diagonal divided by total observations
+        accuracy = np.trace(cf) / float(np.sum(cf))
+
+        # if it is a binary confusion matrix, show some more stats
+        if len(cf) == 2:
+            # Metrics for Binary Confusion Matrices
+            precision = cf[1, 1] / sum(cf[:, 1])
+            recall = cf[1, 1] / sum(cf[1, :])
+            f1_score = 2 * precision * recall / (precision + recall)
+            stats_text = "\n\nAccuracy={:0.3f}\nPrecision={:0.3f}\nRecall={:0.3f}\nF1 Score={:0.3f}".format(
+                accuracy, precision, recall, f1_score)
+        else:
+            stats_text = "\n\nAccuracy={:0.3f}".format(accuracy)
+    else:
+        stats_text = ""
+
+    # SET FIGURE PARAMETERS ACCORDING TO OTHER ARGUMENTS
+    if figsize == None:
+        # Get default figure size if not set
+        figsize = plt.rcParams.get('figure.figsize')
+
+    if xyticks == False:
+        # Do not show categories if xyticks is False
+        categories = False
+
+    # MAKE THE HEATMAP VISUALIZATION
+    fig = plt.figure(figsize=figsize)
+    sns.heatmap(cf, annot=box_labels, fmt="", cmap=cmap, cbar=cbar, xticklabels=categories, yticklabels=categories)
+
+    if xyplotlabels:
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label' + stats_text)
+    else:
+        plt.xlabel(stats_text)
+
+    if title:
+        plt.title(title)
+    return fig
 
 
 def apply_fx(audio, rate: float, board: pdb.Pedalboard):
@@ -29,7 +129,8 @@ def apply_fx(audio, rate: float, board: pdb.Pedalboard):
     return board.process(audio, rate)
 
 
-def read_audio(path: str, normalize: bool = True, add_noise: bool = False, cut_beginning: float = None, **kwargs) -> Tuple[ArrayLike, float]:
+def read_audio(path: str, normalize: bool = True, add_noise: bool = False, cut_beginning: float = None, **kwargs) -> \
+Tuple[ArrayLike, float]:
     """
     Wrapper function to read an audio file using pedalboard.io
     :param path: Path to audio file to read;
@@ -47,7 +148,7 @@ def read_audio(path: str, normalize: bool = True, add_noise: bool = False, cut_b
     if add_noise:
         audio += np.random.normal(0, 1e-9, len(audio))
     if cut_beginning is not None:
-        audio = audio[:, int(cut_beginning*rate):]
+        audio = audio[:, int(cut_beginning * rate):]
     return audio, rate
 
 
