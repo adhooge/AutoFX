c575c86149ee6064f07fc48a5bcb4e40693d704a
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-04-13 17:49:49 +0200

    minor changes

--- source/autofx_train.py
+++ source/autofx_train.py
@@ -6,17 +6,19 @@ from torch.utils.data import random_split, DataLoader
 import pytorch_lightning as pl
 from autofx import AutoFX
 from mbfx_dataset import MBFXDataset
+import pedalboard as pdb
 
-DATASET_PATH = pathlib.Path("/home/alexandre/dataset/mbfx_disto")
-NUM_EPOCHS = 400
+DATASET_PATH = pathlib.Path("/home/alexandre/dataset/mbfx_disto_guitar_mono")
+NUM_EPOCHS = 20
 
 
 logger = TensorBoardLogger("lightning_logs")
-tracker = CarbonTracker(epochs=400, epochs_before_pred=10, monitor_epochs=10, log_dir=logger.log_dir, verbose=2)
+
 # tracker = None
 dataset = MBFXDataset(DATASET_PATH / 'params.csv', DATASET_PATH, rate=44100)
-train, test = random_split(dataset, [15000, 5016])
+train, test = random_split(dataset, [14000, 4720])
 
-cnn = AutoFX(tracker=tracker)
+
+cnn = AutoFX(fx=[pdb.Distortion, pdb.Gain], num_bands=4, tracker=False)
 trainer = pl.Trainer(gpus=1, logger=logger, max_epochs=NUM_EPOCHS)
 trainer.fit(cnn, DataLoader(train, batch_size=64, num_workers=4), DataLoader(test, batch_size=64, num_workers=4))
