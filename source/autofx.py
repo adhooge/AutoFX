6900bb568197c445ae70e60e08e84d9f3486d236
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-04-15 11:35:56 +0200

    [fix]: Remove hardcoded values in `__init__` and `forward`

--- source/autofx.py
+++ source/autofx.py
@@ -5,7 +5,7 @@ Convolutional Neural Network for parameter estimation
 import os
 import pathlib
 import pedalboard as pdb
-from typing import Any, Optional
+from typing import Any, Optional, Tuple
 
 import torchaudio.transforms
 from carbontracker.tracker import CarbonTracker
@@ -16,38 +16,47 @@ from torch import nn
 import pytorch_lightning as pl
 import sys
 from multiband_fx import MultiBandFX
-
+from math import floor
 sys.path.append('..')
 
 
 class AutoFX(pl.LightningModule):
-    def _shape_after_conv(self, x):
+    def _shape_after_conv(self, x: torch.Tensor or Tuple):
         """
         Return shape after Conv2D according to https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
         :param x:
         :return:
         """
-        batch_size, c_in, h_in, w_in = x.shape
-        for conv in self.conv:
-            h_out = torch.floor((h_in + 2*conv.padding[0] - conv.dilation[0]*(conv.kernel_size[0] - 1) - 1)/conv.stride[0] + 1)
-            w_out = torch.floor((w_in + 2*conv.padding[1] - conv.dilation[1]*(conv.kernel_size[1] - 1) - 1)/conv.stride[1] + 1)
+        if isinstance(x, torch.Tensor):
+            batch_size, c_in, h_in, w_in = x.shape
+        else:
+            batch_size, c_in, h_in, w_in = x
+        for seq in self.conv:
+            conv = seq[0]
+            h_out = floor((h_in + 2*conv.padding[0] - conv.dilation[0]*(conv.kernel_size[0] - 1) - 1)/conv.stride[0] + 1)
+            w_out = floor((w_in + 2*conv.padding[1] - conv.dilation[1]*(conv.kernel_size[1] - 1) - 1)/conv.stride[1] + 1)
             h_in = h_out
             w_in = w_out
-        c_out = self.conv[-1].out_channels
+        c_out = self.conv[-1][0].out_channels
         return batch_size, c_out, h_out, w_out
 
     def __init__(self, fx: pdb.Plugin, num_bands: int, tracker: bool = False, rate: int = 16000,
                  conv_ch: list[int] = [6, 16, 32], conv_k: list[int] = [5, 5, 5],
                  fft_size: int = 1024, hop_size: int = 256, audiologs: int = 8,
                  mrstft_fft: list[int] = [64, 128, 256, 512, 1024, 2048],
-                 mrstft_hop: list[int] = [16, 32, 64, 128, 256, 512]):
+                 mrstft_hop: list[int] = [16, 32, 64, 128, 256, 512],
+                 spectro_power: int = 2, hidden_size: int = 512):
         # TODO: Make attributes changeable from arguments
         super().__init__()
         self.conv = []
+        self.mbfx = MultiBandFX(fx, num_bands)
+        self.num_params = num_bands * len(self.mbfx.settings[0])
         for c in range(len(conv_ch)):
             self.conv.append(nn.Sequential(nn.Conv2d(1, conv_ch[c], conv_k[c]), nn.BatchNorm2d(conv_ch[c]), nn.ReLU()))
-        self.gru = nn.GRU(16032, 512, batch_first=True)
-        self.fcl = nn.Linear(512, 8)
+        _, _, h_out, _ = self._shape_after_conv((None, None, fft_size//2 + 1, 0))
+        self.gru = nn.GRU(h_out * conv_ch[-1], hidden_size, batch_first=True)
+        self.hidden_size = hidden_size
+        self.fcl = nn.Linear(hidden_size, self.num_params)
         self.activation = nn.Sigmoid()
         self.loss = nn.L1Loss()
         self.tracker_flag = tracker
@@ -57,9 +66,8 @@ class AutoFX(pl.LightningModule):
                                                             mrstft_fft,
                                                             device="cpu")       # TODO: Manage device properly
         self.num_bands = num_bands
-        self.mbfx = MultiBandFX(fx, num_bands)
         self.rate = rate
-        self.spectro = torchaudio.transforms.Spectrogram(n_fft=fft_size, hop_length=hop_size, power=2)
+        self.spectro = torchaudio.transforms.Spectrogram(n_fft=fft_size, hop_length=hop_size, power=spectro_power)
         self.inv_spectro = torchaudio.transforms.InverseSpectrogram(n_fft=fft_size, hop_length=hop_size)
         self.audiologs = audiologs
 
@@ -70,7 +78,7 @@ class AutoFX(pl.LightningModule):
         batch_size, channels, h_out, w_out = x.shape
         x = x.view(batch_size, w_out, channels, h_out)
         x = x.view(batch_size, w_out, -1)
-        x, _ = self.gru(x, torch.zeros(1, batch_size, 512, device=x.device))
+        x, _ = self.gru(x, torch.zeros(1, batch_size, self.hidden_size, device=x.device))
         x = self.fcl(x)
         x = torch.mean(x, 1)
         x = self.activation(x)
@@ -80,7 +88,7 @@ class AutoFX(pl.LightningModule):
         data, label = batch
         batch_size = data.shape[0]
         pred = self.forward(data)
-        rec = torch.zeros(batch_size, 32000)
+        rec = torch.zeros(batch_size, 32000)        # TODO: Remove hardcoded values
         for (i, snd) in enumerate(data):
             for b in range(self.num_bands):
                 # TODO: Make it fx agnostic
@@ -123,7 +131,7 @@ class AutoFX(pl.LightningModule):
     def on_train_epoch_start(self) -> None:
         if self.tracker_flag and self.tracker is None:
             self.tracker = CarbonTracker(epochs=400, epochs_before_pred=10, monitor_epochs=10,
-                                         log_dir=self.logger.log_dir, verbose=2)
+                                         log_dir=self.logger.log_dir, verbose=2)            # TODO: Remove hardcoded values
         if self.tracker_flag:
             self.tracker.epoch_start()
 
@@ -132,5 +140,5 @@ class AutoFX(pl.LightningModule):
             self.tracker.epoch_end()
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)           # TODO: Remove hardcoded values
         return optimizer
