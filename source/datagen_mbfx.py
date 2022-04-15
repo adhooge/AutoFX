c5b2b86ecc9ddf62f672100353731310e55e8636
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-04-15 12:42:43 +0200

    [mod]: return clean sound along with processed sound

--- source/datagen_mbfx.py
+++ source/datagen_mbfx.py
@@ -13,32 +13,34 @@ from tqdm.auto import tqdm
 
 # warnings.filterwarnings("ignore", category=FutureWarning)
 
-DATA_PATH = pathlib.Path("/home/alexandre/dataset/full_idmt_dry")
+DATA_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_dry")
 NUM_BANDS = 4
 FX = [pdb.Distortion, pdb.Gain]
 DRIVE_MIN = 10
 DRIVE_MAX = 60
 GAIN_MIN = -10
 GAIN_MAX = 10
-OUT_PATH = pathlib.Path("/home/alexandre/dataset/mbfx_disto")
+OUT_PATH = pathlib.Path("/home/alexandre/dataset/mbfx_disto_guitar_mono")
 
-NUM_RUNS = 4
+NUM_RUNS = 10
+
+# TODO: Could automatically adapt to new FX
 
 dataframe = pd.DataFrame(columns=['drive_level0', 'drive_level1', 'drive_level2', 'drive_level3',
                                   'gain_level0', 'gain_level1', 'gain_level2', 'gain_level3'], dtype='float64')
-mbfx = MultiBandFX(FX, NUM_BANDS)
+# mbfx = MultiBandFX(FX, NUM_BANDS)
 for i in tqdm(range(NUM_RUNS), position=1):
-    for file in tqdm(DATA_PATH.iterdir(), total=5004, position=0, leave=True):
+    for file in tqdm(DATA_PATH.iterdir(), total=1872, position=0, leave=True):
         drive_levels = np.array([random.uniform(DRIVE_MIN, DRIVE_MAX) for b in range(NUM_BANDS)])
         gain_levels = np.array([random.uniform(GAIN_MIN, GAIN_MAX) for b in range(NUM_BANDS)])
-        ground_truth = np.hstack(((drive_levels - 10)/50, (gain_levels/20 + 0.5)))
-        for b in range(NUM_BANDS):
-            mbfx.mbfx[b][0].drive_db = drive_levels[b]
-            mbfx.mbfx[b][1].gain_db = gain_levels[b]
-        audio, rate = util.read_audio(file, normalize=True, cut_beginning=0.45, add_noise=True)
-        audio = mbfx.process(audio, rate)
-        dataframe.loc[file.stem + '_' + str(i)] = ground_truth
-        sf.write(OUT_PATH / (file.stem + '_' + str(i) + file.suffix), audio.T, int(rate))
+        normalized_levels = np.hstack(((drive_levels - 10)/50, (gain_levels/20 + 0.5)))
+        # for b in range(NUM_BANDS):
+        #    mbfx.mbfx[b][0].drive_db = drive_levels[b]
+        #    mbfx.mbfx[b][1].gain_db = gain_levels[b]
+        # audio, rate = util.read_audio(file, normalize=True, add_noise=True)
+        # audio = mbfx.process(audio, rate)
+        dataframe.loc[file.stem + '_' + str(i)] = normalized_levels
+        # sf.write(OUT_PATH / (file.stem + '_' + str(i) + file.suffix), audio.T, int(rate))
     dataframe.to_csv(OUT_PATH / "params.csv")
     dataframe.to_pickle(OUT_PATH / "params.pkl")
 
--- source/mbfx_dataset.py
+++ source/mbfx_dataset.py
@@ -2,25 +2,29 @@
 Dataset class for spectrogram from audio.
 """
 import pathlib
+from typing import List
+
+import pedalboard as pdb
 
 import torch
 import torchaudio
 import pandas as pd
 from torch import nn
 from torch.utils.data import Dataset
-
+from multiband_fx import MultiBandFX
 import util
 
 
 class MBFXDataset(Dataset):
-    def __init__(self, labels_file: str or pathlib.Path,
-                 snd_dir: str or pathlib.Path, rate: int,
+    def __init__(self, params_file: str or pathlib.Path,
+                 cln_snd_dir: str or pathlib.Path, prc_snd_dir: str or pathlib.Path, rate: int,
                  resampling_rate: int = 16000, **kwargs):
-        self.snd_labels = pd.read_csv(labels_file)
-        self.snd_dir = pathlib.Path(snd_dir)
+        self.snd_labels = pd.read_csv(params_file)
+        self.cln_snd_dir = pathlib.Path(cln_snd_dir)
+        self.prc_snd_dir = pathlib.Path(prc_snd_dir)
         self.transform = nn.Sequential(
-                            torchaudio.transforms.Resample(rate, resampling_rate)
-                            )
+            torchaudio.transforms.Resample(rate, resampling_rate)
+        )
 
     def __len__(self):
         return len(self.snd_labels)
@@ -28,9 +32,12 @@ class MBFXDataset(Dataset):
     def __getitem__(self, item):
         if torch.is_tensor(item):
             item = item.tolist()
-        snd_path = self.snd_dir / (self.snd_labels.iloc[item, 0] + '.wav')
-        sound, rate = torchaudio.load(snd_path, normalize=True)
-        label = self.snd_labels.iloc[item, 1:]
-        label = torch.Tensor(label)
-        spectro = self.transform(sound)
-        return spectro, label
+        cln_snd_path = self.cln_snd_dir / (self.snd_labels.iloc[item, 0].split('_')[0] + '.wav')
+        prc_snd_path = self.prc_snd_dir / (self.snd_labels.iloc[item, 0] + '.wav')
+        cln_sound, rate = torchaudio.load(cln_snd_path, normalize=True)
+        prc_sound, rate = torchaudio.load(prc_snd_path, normalize=True)
+        params = self.snd_labels.iloc[item, 1:]
+        params = torch.Tensor(params)
+        cln_resampled = self.transform(cln_sound)
+        prc_resampled = self.transform(prc_sound)
+        return cln_resampled, prc_resampled, params
