e3ffccf7ff273e33c9ddff8cc4d14dd4f81861c3
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-05-23 18:17:10 +0200

    :zap: Multi-gpu compatibility

--- source/cafx_train.py
+++ source/cafx_train.py
@@ -14,7 +14,7 @@ OUT_OF_DOMAIN_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_modulatio
 NUM_EPOCHS = 200
 
 PARAM_RANGE = [(0.1, 10), (0, 1), (0, 20), (0, 1), (0, 1)]
-CHECKPOINT = pathlib.Path("/home/alexandre/logs/cafx/version_0/checkpoints/epoch=49-step=46800.ckpt")
+CHECKPOINT = pathlib.Path("/home/alexandre/logs/cafx/version_1/checkpoints/epoch=99-step=93600.ckpt")
 logger = TensorBoardLogger("/home/alexandre/logs", name="cafx")
 
 datamodule = FeaturesDataModule(CLEAN_PATH, PROCESSED_PATH, OUT_OF_DOMAIN_PATH, seed=2)
@@ -24,7 +24,8 @@ cafx = CAFx([pdb.Chorus], 1, PARAM_RANGE, 28, loss_stamps=[50, 200])
 
 checkpoint_callback = ModelCheckpoint(every_n_epochs=5, save_top_k=-1)
 
-trainer = pl.Trainer(gpus=1, logger=logger, max_epochs=NUM_EPOCHS, auto_select_gpus=True,
+trainer = pl.Trainer(gpus=2, logger=logger, max_epochs=NUM_EPOCHS,
+                     strategy= 'ddp', auto_select_gpus=True,
                      callbacks=[checkpoint_callback], log_every_n_steps=100, detect_anomaly=True)
 trainer.fit(cafx, datamodule=datamodule,
             ckpt_path=CHECKPOINT)
