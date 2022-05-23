990d2cbaf7fa13e048381e38932882f86815eae9
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-05-23 10:31:22 +0200

    :speech_balloon: fix typehinting

--- source/features_linear_nn.py
+++ source/features_linear_nn.py
@@ -23,7 +23,7 @@ class AutoFxLite(pl.LightningModule):
                  audiologs: int = 4, loss_weights: list[float] = [1, 1],
                  mrstft_fft: list[int] = [256, 512, 1024, 2048],
                  mrstft_hop: list[int] = [64, 128, 256, 512],
-                 learning_rate: int = 0.001, out_of_domain: bool = False,
+                 learning_rate: float = 0.001, out_of_domain: bool = False,
                  spectro_power: int = 2, mel_spectro: bool = True, mel_num_bands: int = 128,
                  loss_stamps: list = None, device=torch.device('cuda'),
                  *args: Any, **kwargs: Any):
@@ -58,20 +58,40 @@ class AutoFxLite(pl.LightningModule):
         self.loss_stamps = loss_stamps
         self.mbfx_layer = MBFxLayer(self.mbfx, self.rate, self.param_range, fake_num_bands=self.num_bands)
         self.model = nn.Sequential(
-            nn.Linear(in_features, in_features, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(in_features, in_features // 2, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(in_features // 2, self.num_params, bias=False)
+            nn.LayerNorm([in_features]),
+            nn.Linear(in_features, in_features, bias=True),
+            nn.Dropout(),
+            nn.BatchNorm1d(in_features),
+            nn.Linear(in_features, in_features, bias=True),
+            nn.Dropout(),
+            nn.BatchNorm1d(in_features),
+            nn.Linear(in_features, in_features, bias=True),
+            nn.Dropout(),
+            nn.BatchNorm1d(in_features),
+            # nn.LeakyReLU(inplace=True),
+            nn.Linear(in_features, in_features, bias=True),
+            nn.Dropout(),
+            nn.BatchNorm1d(in_features),
+            # nn.LeakyReLU(inplace=True),
+            nn.Linear(in_features, in_features, bias=True),
+            nn.Dropout(),
+            nn.BatchNorm1d(in_features),
+            # nn.LeakyReLU(inplace=True),
+            nn.Linear(in_features, in_features // 2, bias=True),
+            nn.Dropout(),
+            nn.BatchNorm1d(in_features//2),
+            # nn.LeakyReLU(inplace=True),
+            nn.Linear(in_features // 2, self.num_params, bias=True)
         )
 
     def forward(self, x, *args, **kwargs) -> Any:
-        out = self.model(x)
-        out = self.activation(out)
-        return out
+        out1 = self.model(x)
+        out2 = self.activation(out1)
+        return out2
 
     def training_step(self, batch, batch_idx, *args, **kwargs) -> STEP_OUTPUT:
         # TODO: Accept spectral loss
+        self.tmp = False
         _, _, label, features = batch
         pred = self.forward(features)
         loss = self.loss(pred, label)
@@ -95,19 +115,21 @@ class AutoFxLite(pl.LightningModule):
         self.logger.experiment.add_scalars("Param_distance/test", scalars, global_step=self.global_step)
         pred = pred.to("cpu")
         rec = torch.zeros(batch_size, clean.shape[-1], device=self.device)  # TODO: fix hardcoded value
-        for (i, snd) in enumerate(clean):
-            if i > self.audiologs:
-                break
-            self.mbfx_layer.params = nn.Parameter(pred[i])
-            rec[i] = self.mbfx_layer.forward(snd)
-        for l in range(self.audiologs):
-            self.logger.experiment.add_audio(f"Audio/{l}/Original", processed[l] / torch.max(torch.abs(processed[l])),
-                                             sample_rate=self.rate, global_step=self.global_step)
-            self.logger.experiment.add_audio(f"Audio/{l}/Matched", rec[l] / torch.max(torch.abs(rec[l])),
-                                             sample_rate=self.rate, global_step=self.global_step)
-            self.logger.experiment.add_text(f"Audio/{l}/Matched_params", str(pred[l]), global_step=self.global_step)
-            self.logger.experiment.add_text(f"Audio/{l}/Original_params", str(label[l]),
-                                            global_step=self.global_step)
+        if not self.tmp:
+            self.tmp = True
+            for (i, snd) in enumerate(clean):
+                if i > self.audiologs:
+                    break
+                self.mbfx_layer.params = nn.Parameter(pred[i])
+                rec[i] = self.mbfx_layer.forward(snd)
+            for l in range(self.audiologs):
+                self.logger.experiment.add_audio(f"Audio/{l}/Original", processed[l] / torch.max(torch.abs(processed[l])),
+                                                 sample_rate=self.rate, global_step=self.global_step)
+                self.logger.experiment.add_audio(f"Audio/{l}/Matched", rec[l] / torch.max(torch.abs(rec[l])),
+                                                 sample_rate=self.rate, global_step=self.global_step)
+                self.logger.experiment.add_text(f"Audio/{l}/Matched_params", str(pred[l]), global_step=self.global_step)
+                self.logger.experiment.add_text(f"Audio/{l}/Original_params", str(label[l]),
+                                                global_step=self.global_step)
         return loss
 
     def configure_optimizers(self):
@@ -120,7 +142,7 @@ if __name__ == '__main__':
     PROCESSED_PATH = pathlib.Path("/home/alexandre/dataset/mbfx_disto_guitar_mono_int")
     FEATURES_PATH = pathlib.Path("/home/alexandre/dataset/mbfx_disto_guitar_mono_int_features/dataset.csv")
     # PROCESSED_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_non-linear_22050")
-    NUM_EPOCHS = 50
+    NUM_EPOCHS = 100
 
     logger = TensorBoardLogger("/home/alexandre/logs", name="features_linear")
 
@@ -131,13 +153,14 @@ if __name__ == '__main__':
     # train, test = random_split(dataset, [2808, 936])
 
     nn_linear = AutoFxLite(fx=[pdb.Distortion(0), pdb.Gain(0)], num_bands=4, total_num_bands=4,
-                    param_range=[(10, 60), (-10, 10)], audiologs=6)
+                    param_range=[(10, 60), (-10, 10)], audiologs=6, learning_rate=0.1)
 
     checkpoint_callback = ModelCheckpoint(every_n_epochs=5, save_top_k=-1)
-    trainer = pl.Trainer(gpus=1, logger=logger, max_epochs=NUM_EPOCHS, auto_lr_find=True, auto_select_gpus=True,
+    trainer = pl.Trainer(gpus=1, logger=logger, max_epochs=NUM_EPOCHS, auto_select_gpus=True,
                          callbacks=[checkpoint_callback], log_every_n_steps=100, track_grad_norm=2, detect_anomaly=True)
-    trainer.fit(nn_linear, DataLoader(train, batch_size=64, num_workers=4, shuffle=True),
-                DataLoader(test, batch_size=64, num_workers=4, shuffle=True))
+    trainer.fit(nn_linear, DataLoader(train, batch_size=256, num_workers=6, shuffle=True),
+                DataLoader(test, batch_size=256, num_workers=6, shuffle=True),
+                ckpt_path="/home/alexandre/logs/features_linear/version_4/checkpoints/epoch=49-step=5500.ckpt")
     # trainer = pl.Trainer()
     # trainer.fit(cnn, DataLoader(train, batch_size=64, num_workers=4, shuffle=True),
     #            DataLoader(test, batch_size=64, num_workers=4, shuffle=True),
