cbefbd0b70a03239c9de7c92b24dac08afb5d7e7
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-05-23 14:27:39 +0200

    :ambulance: Corrected one_hot encoding

--- source/classifier_pytorch.py
+++ source/classifier_pytorch.py
@@ -4,6 +4,7 @@ import pandas as pd
 import torch
 from pytorch_lightning.utilities.types import STEP_OUTPUT
 from torch import nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 from sklearn.model_selection import train_test_split
@@ -14,13 +15,16 @@ from ignite.metrics.confusion_matrix import ConfusionMatrix
 
 from pytorch_lightning.loggers import TensorBoardLogger
 
-import util
+import source.util as util
 
 
 class TorchStandardScaler:
     """
     from    https://discuss.pytorch.org/t/pytorch-tensor-scaling/38576/8
     """
+    def __init__(self):
+        self.mean = 0
+        self.std = 1
 
     def fit(self, x):
         self.mean = x.mean(0, keepdim=True)
@@ -58,10 +62,9 @@ class MLPClassifier(pl.LightningModule):
             raise NotImplementedError
 
     @staticmethod
-    def _to_one_hot(batch):
-        out = torch.zeros_like(batch)
-        idx = torch.argmax(batch, dim=-1)
-        out[idx] = 1
+    def _to_one_hot(batch, num_classes):
+        idx = torch.argmax(batch, dim=-1, keepdim=False)
+        out = F.one_hot(idx, num_classes=num_classes)
         return out
 
     def __init__(self, input_size: int, output_size: int,
@@ -77,8 +80,8 @@ class MLPClassifier(pl.LightningModule):
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
         self.loss = nn.CrossEntropyLoss()
-        self.softmax = nn.Softmax()
-        self.precision = Precision()
+        self.softmax = nn.Softmax(dim=-1)
+        self.prec = Precision()
         self.recall = Recall()
         self.accuracy = Accuracy()
         self.confusion_matrix = ConfusionMatrix(num_classes=output_size)
@@ -96,23 +99,32 @@ class MLPClassifier(pl.LightningModule):
         pred = self.forward(feat)
         loss = self.loss(pred, label)
         self.logger.experiment.add_scalar("Cross-entropy loss", loss, global_step=self.global_step)
-        classes = MLPClassifier._to_one_hot(pred)
-        self.precision.reset()
+        classes = MLPClassifier._to_one_hot(pred, self.output_size)
+        self.prec.reset()
         self.accuracy.reset()
         self.recall.reset()
         self.confusion_matrix.reset()
-        self.precision.update((classes, label))
+        self.prec.update((classes, label))
         self.accuracy.update((classes, label))
         self.recall.update((classes, label))
         self.confusion_matrix.update((classes, label))
-        precision = self.precision.compute()
-        print("Precision: ", precision)
+        precision = self.prec.compute()
+        self.logger.experiment.add_scalars("Metrics/Precision",
+                                           dict(zip(CLASSES, precision)),
+                                           global_step=self.global_step)
         accuracy = self.accuracy.compute()
-        print("Accuracy: ", accuracy)
+        self.logger.experiment.add_scalar("Metrics/Accuracy",
+                                           accuracy,
+                                           global_step=self.global_step)
         recall = self.recall.compute()
-        print("Recall: ", recall)
+        self.logger.experiment.add_scalars("Metrics/Recall",
+                                           dict(zip(CLASSES, recall)),
+                                           global_step=self.global_step)
         confusion_matrix = self.confusion_matrix.compute()
-        print("Conf Matrix: ", confusion_matrix)
+        fig = util.make_confusion_matrix(confusion_matrix.numpy(),
+                                         group_names=CLASSES)
+        self.logger.experiment.add_figure("Metrics/Confusion_matrix",
+                                          fig, global_step=self.global_step)
         return loss
 
     def configure_optimizers(self):
@@ -136,11 +148,13 @@ data = subset.drop(columns=['target_name'])
 print(data)
 
 X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3, random_state=2)
+X_train, X_test = torch.tensor(X_train.values, dtype=torch.float), torch.tensor(X_test.values, dtype=torch.float)
+y_train, y_test = torch.tensor(y_train), torch.tensor(y_test)
 
 scaler = TorchStandardScaler()
 scaler.fit(X_train)
-X_train_scaled = scaler.transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+X_train_scaled = scaler.transform(X_train.clone())
+X_test_scaled = scaler.transform(X_test.clone())
 
 train_dataset = ClassificationDataset(X_train_scaled, y_train)
 test_dataset = ClassificationDataset(X_test_scaled, y_test)
@@ -149,11 +163,12 @@ train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True,
 test_dataloader = DataLoader(test_dataset, batch_size=256, num_workers=4)
 
 
-clf = MLPClassifier(254, len(CLASSES), 100, activation='sigmoid', solver='adam',
+clf = MLPClassifier(len(data.columns), len(CLASSES), 100, activation='sigmoid', solver='adam',
                     max_iter=500)
 
 logger = TensorBoardLogger("/home/alexandre/logs", name="classifier")
-trainer = pl.Trainer(gpus=2, logger=logger, max_epochs=clf.max_iter,
+trainer = pl.Trainer(gpus=1, logger=logger, max_epochs=clf.max_iter,
+                     accelerator='ddp',
                      auto_select_gpus=True, log_every_n_steps=10)
 
 trainer.fit(clf, train_dataloader, test_dataloader)
