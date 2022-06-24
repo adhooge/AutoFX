3a350356becb4bb05d407bfb7827f81518574438
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-06-24 12:09:17 +0200

    :heavy_plus_sign: early_stopping and save scaler

--- source/classifiers/classifier_train.py
+++ source/classifiers/classifier_train.py
@@ -1,4 +1,8 @@
+import pickle
+
 import pandas as pd
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+
 import source.util as util
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
@@ -12,8 +16,8 @@ import source.classifiers.classifier_pytorch as torch_clf
 CLASSES = ['Dry', 'Feedback Delay', 'Slapback Delay', 'Reverb',
            'Chorus', 'Flanger', 'Phaser',
            'Tremolo', 'Vibrato', 'Distortion', 'Overdrive']
-
-dataset = pd.read_csv('/home/alexandre/dataset/IDMT_FULL_CUT_22050/out.csv', index_col=0)
+OUT_PATH = "/home/alexandre/logs/classif24juin"
+dataset = pd.read_csv('/home/alexandre/dataset/IDMT_guitar_mono_CUT_22050/out.csv', index_col=0)
 subset = dataset.drop(columns=['file'])
 target = subset['class']
 data = subset.drop(columns=['class'])
@@ -22,7 +26,7 @@ print(data)
 X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1, random_state=2,
                                                     stratify=target)
 
-sss = StratifiedShuffleSplit(n_splits=9, test_size=1 / 9, random_state=2)
+sss = StratifiedShuffleSplit(n_splits=5, test_size=1 / 5, random_state=2)
 i = 0
 for train_index, valid_index in sss.split(X_train, y_train):
     print("Working on fold", i)
@@ -43,20 +47,25 @@ for train_index, valid_index in sss.split(X_train, y_train):
 
     train_dataset = torch_clf.ClassificationDataset(X_train_scaled, y_train_cv)
     valid_dataset = torch_clf.ClassificationDataset(X_valid_scaled, y_valid_cv)
-    train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True,
+    train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True,
                                   num_workers=4)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=256, num_workers=4)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=128, num_workers=4)
 
     clf = torch_clf.MLPClassifier(len(data.columns), len(CLASSES), 100, activation='sigmoid', solver='adam',
                                   max_iter=200, learning_rate=0.002)
 
-    logger = TensorBoardLogger("/home/alexandre/logs/classif21juin", name="9fold_cross-validation")
+    logger = TensorBoardLogger(OUT_PATH, name="guitar_Mono")
     # early_stop_callback = EarlyStopping(monitor="train_loss",
     #                                     min_delta=clf.tol,
     #                                    patience=clf.n_iter_no_change)
+
+    early_stopping = EarlyStopping(monitor='loss/test', patience=10)
+    checkpoint_callback = ModelCheckpoint(save_top_k=3, monitor='loss/test')
     trainer = pl.Trainer(gpus=1, logger=logger, max_epochs=clf.max_iter,
                          accelerator='ddp',
                          auto_select_gpus=True, log_every_n_steps=10,
-                         )  # callbacks=[early_stop_callback])
+                         callbacks=[checkpoint_callback, early_stopping])
 
     trainer.fit(clf, train_dataloader, valid_dataloader)
+    with open(logger.log_dir + '/scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
