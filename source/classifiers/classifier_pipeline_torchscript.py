20c55e289817dd11900b58d5f52cf8957340c59d
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-07-12 14:28:18 +0200

    :heavy_plus_sign: compile model with or without argmax

--- source/classifiers/classifier_pipeline_torchscript.py
+++ source/classifiers/classifier_pipeline_torchscript.py
@@ -1,3 +1,5 @@
+import pickle
+
 import pandas as pd
 import torch
 from sklearn.model_selection import train_test_split
@@ -8,22 +10,23 @@ from source.classifiers.classifier_pytorch import MLPClassifier
 
 import source.classifiers.classifier_pytorch as torch_clf
 
-CHECKPOINT = "/home/alexandre/logs/classif/torch/version_0/checkpoints/epoch=999-step=151000.ckpt"
+CHECKPOINT = "/home/alexandre/logs/classif4july/guitar_Mono/version_1/checkpoints/epoch=66-step=20770.ckpt"
 
-clf = MLPClassifier.load_from_checkpoint(CHECKPOINT, input_size=143,
+clf = MLPClassifier.load_from_checkpoint(CHECKPOINT, input_size=163,
                                          output_size=11, hidden_size=100,
                                          activation='sigmoid', solver='adam',
                                          max_iter=1000)
 
 
 class ClassifierPipeline(nn.Module):
-    def __init__(self, classifier, feat_extractor, scaler, remover):
+    def __init__(self, classifier, feat_extractor, scaler, remover, with_argmax: bool = True):
         super(ClassifierPipeline, self).__init__()
         self.clf = classifier
         # self.feat_extractor = torch.jit.trace(feat_extractor, example)
         self.feat_extractor = feat_extractor
         self.scaler = scaler
         self.remover = remover
+        self.with_argmax = with_argmax
         self.pipeline = nn.Sequential(
             remover,
             feat_extractor,
@@ -36,10 +39,12 @@ class ClassifierPipeline(nn.Module):
         feat = self.feat_extractor(cut_audio)
         scaled_feat = self.scaler(feat)
         pred = self.clf(scaled_feat)
-        return torch.argmax(pred)
+        if self.with_argmax:
+            return torch.argmax(pred)
+        return pred
 
 
-dataset = pd.read_csv('/home/alexandre/dataset/IDMT_FULL_CUT_22050/out.csv', index_col=0)
+dataset = pd.read_csv('/home/alexandre/dataset/IDMT_guitar_mono_CUT_22050/out.csv', index_col=0)
 subset = dataset.drop(columns=['file'])
 target = subset['class']
 data = subset.drop(columns=['class'])
@@ -60,4 +65,19 @@ train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True,
                               num_workers=4)
 test_dataloader = DataLoader(test_dataset, batch_size=256, num_workers=4)
 
+with open("/home/alexandre/logs/classif4july/guitar_Mono/version_1/scaler.pkl", 'rb') as f:
+    scaler = pickle.load(f)
 
+
+scaler_mod = torch_clf.ScalerModule()
+scaler_mod.mean = scaler.mean
+scaler_mod.std = scaler.std
+extractor = torch_clf.FeatureExtractor()
+cutter = torch_clf.SilenceRemover(0.05, 0.95)
+pipe = ClassifierPipeline(clf, extractor, scaler_mod, cutter, with_argmax=False)
+
+
+example = torch.randn((1, 44100))
+p = torch.jit.trace(pipe, example)
+
+torch.jit.save(p, "/home/alexandre/logs/classif4july/guitar_Mono/version_1/pipe_noArgmax.pt")
