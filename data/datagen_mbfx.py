b505097c11f746204b129eec7aa12a8f6a348975
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-07-21 17:36:42 +0200

    :heavy_plus_sign: CustomDistortion compat

--- data/datagen_mbfx.py
+++ data/datagen_mbfx.py
@@ -12,23 +12,30 @@ import pedalboard as pdb
 import pandas as pd
 import soundfile as sf
 from tqdm.auto import tqdm
+from source.models.custom_distortion import CustomDistortion
 
 # warnings.filterwarnings("ignore", category=FutureWarning)
 
 DATA_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_dry_22050_cut")
 NUM_BANDS = 1
 NUM_CHANGED_BANDS = 1
-FX = [pdb.Chorus()]
-PARAM_RANGE = [(0.1, 10), (0, 1), (0, 20), (0, 1), (0, 1)]
-PARAMS = ["p-rate_hz", "p-depth", "p-centre_delay_ms", "p-feedback", "p-mix"]
-OUT_PATH = pathlib.Path("/home/alexandre/dataset/modulation_guitar_mono_cut")
+# FX = [pdb.Delay()]
+FX = CustomDistortion()
+PARAM_RANGE = [(0, 60),
+               (50, 500), (-10, 10), (0.5, 2),
+               (500, 2000), (-10, 10), (0.5, 2)]
+PARAMS = ["p-drive_db",
+          "p-cutoff_frequency_hz_lo", "p-gain_db_lo", "p-q_lo",
+          "p-cutoff_frequency_hz_hi", "p-gain_db_hi", "p-q_hi"]
+OUT_PATH = pathlib.Path("/home/alexandre/dataset/distortion_guitar_mono_cut2")
 
 NUM_RUNS = 20
 
 # TODO: Could automatically adapt to new FX
 
 dataframe = pd.DataFrame(columns=PARAMS, dtype='float64')
-mbfx = MultiBandFX(FX, NUM_BANDS)
+# mbfx = MultiBandFX(FX, NUM_BANDS)
+mbfx = FX
 if not(OUT_PATH.exists()):
     os.mkdir(OUT_PATH)
 if (OUT_PATH / "params.csv").exists():
