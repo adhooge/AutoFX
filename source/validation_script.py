2f13ce040d43812c09efe873c541e0462dcffaf7
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-07-21 17:51:26 +0200

    :heavy_plus_sign: validation scripts

--- source/validation_script.py
+++ source/validation_script.py
@@ -1,3 +1,5 @@
+import tqdm
+
 from source.models.CAFx import CAFx
 from source.models.autofx_resnet import AutoFX
 import pedalboard as pdb
@@ -11,12 +13,12 @@ import torch
 import soundfile as sf
 
 WRITE_AUDIO = True
-PARAM_RANGE = [(0.1, 10), (0, 1), (0, 20), (0, 1), (0, 1)]
-CHECKPOINT = "/home/alexandre/logs/finetuning29juin/lightning_logs/version_12/checkpoints/epoch=114-step=48210.ckpt"
+PARAM_RANGE = [(0, 1), (0, 1), (0, 1)]
+CHECKPOINT = "/home/alexandre/logs/evalFeat19july/lightning_logs/version_0/checkpoints/epoch=14-step=7020.ckpt"
 # CHECKPOINT = "/home/alexandre/logs/resnet_chorus/version_50/checkpoints/epoch=39-step=35000.ckpt"
 CLEAN_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_dry_22050_cut")
-PROCESSED_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_modulation_22050_cut")
-IN_DOMAIN_PATH = pathlib.Path("/home/alexandre/dataset/modulation_guitar_mono_cut")
+PROCESSED_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_delay_22050_cut")
+IN_DOMAIN_PATH = pathlib.Path("/home/alexandre/dataset/delay_guitar_mono_cut")
 out_of_domain_dataset = FeatureOutDomainDataset(PROCESSED_PATH, CLEAN_PATH, PROCESSED_PATH,
                                                 index_col=0)
 in_domain_dataset = FeatureInDomainDataset(IN_DOMAIN_PATH, True, CLEAN_PATH, IN_DOMAIN_PATH)
@@ -25,8 +27,8 @@ in_domain_dataset = FeatureInDomainDataset(IN_DOMAIN_PATH, True, CLEAN_PATH, IN_
 datamodule = FeaturesDataModule(CLEAN_PATH, IN_DOMAIN_PATH, PROCESSED_PATH, seed=2)
 datamodule.setup()
 model = CAFx.load_from_checkpoint(CHECKPOINT,
-                                  fx=[pdb.Chorus], num_bands=1, cond_feat=28,
-                                  param_range=PARAM_RANGE)
+                                  fx=[pdb.Delay], num_bands=1, cond_feat=38,
+                                  param_range=PARAM_RANGE, disable_feat=True)
 
 # model = AutoFX.load_from_checkpoint(CHECKPOINT,
 #                                    fx=[pdb.Chorus], num_bands=1,
@@ -42,8 +44,8 @@ if WRITE_AUDIO:
     pred = model.forward(processed, feat)
     # print(torch.square(pred - label))
     toSave = torch.zeros((1, 64 * 35000))
-    for i in range(32):
+    for i in tqdm.tqdm(range(32)):
         rec = model.mbfx_layer.forward(clean[i], pred[i])
         toSave[0, (2*i)*35000:(2*i + 1)*35000] = processed[i]
         toSave[0, (2 * i + 1) * 35000:2 * (i + 1) * 35000] = rec / (torch.max(torch.abs(rec)))
-    sf.write("/home/alexandre/Music/finetuned29juin_12.wav", toSave.T, 22050)
+    sf.write("/home/alexandre/Music/delay_noFeat_ood.wav", toSave.T, 22050)
