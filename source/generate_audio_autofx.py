c040b92f552eda95af62e8da40713bf0b8662c21
Alexandre D'Hooge <alexandre.dhooge@zaclys.net>
2022-08-01 18:21:17 +0200

    :heavy_plus_sign: spectral loss

--- source/generate_audio_autofx.py
+++ source/generate_audio_autofx.py
@@ -1,3 +1,4 @@
+import auraloss
 import tqdm
 
 from source.models.AutoFX import AutoFX
@@ -23,7 +24,21 @@ CLEAN_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_dry_22050_cut")
 PROCESSED_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_modulation_delay_distortion_22050_cut")
 IN_DOMAIN_PATH = pathlib.Path("/home/alexandre/dataset/modulation_delay_distortion_guitar_mono_cut")
 
-SAVE_PATH = pathlib.Path("/home/alexandre/Music/perceptual_exp/param_no_finetuning")
+# SAVE_PATH = pathlib.Path("/home/alexandre/Music/perceptual_exp/param_no_finetuning")
+
+SAVE_PATH = pathlib.Path('/tmp')
+
+mrstft_fft = [64, 128, 256, 512, 1024, 2048]
+mrstft_hop = [16, 32, 64, 128, 256, 512]
+
+mrstft = auraloss.freq.MultiResolutionSTFTLoss(fft_sizes=mrstft_fft,
+                                               hop_sizes=mrstft_hop,
+                                               win_lengths=mrstft_fft,
+                                               w_phs=1,
+                                               w_lin_mag=0,
+                                               w_sc=0)
+spectral_loss = mrstft
+temporal_loss = torch.nn.MSELoss()
 
 OUT_OF_DOMAIN = True
 
@@ -41,7 +56,6 @@ datamodule = FeaturesDataModule(CLEAN_PATH, IN_DOMAIN_PATH, PROCESSED_PATH, seed
                                 batch_size=128, return_file_name=True
                                 )
 datamodule.setup()
-
 
 # model = AutoFX.load_from_checkpoint(CHECKPOINT,
 #                                    fx=[pdb.Chorus], num_bands=1,
@@ -62,16 +76,31 @@ if WRITE_AUDIO:
         print(i, cnt_modulation, cnt_delay, cnt_disto)
         if filename[i].split('-')[2][1:3] in ["31", "32", "33", "35"] and cnt_modulation != 10:
             rec = model.board_layers[0].forward(clean[i], pred[i, :5])
+            rec = rec / torch.max(torch.abs(rec))
+            print('max processed', torch.max(torch.abs(processed[i])))
+            print('max rec', torch.max(torch.abs(rec)))
+            print("Spectral loss: ", spectral_loss(processed[i], rec))
+            print("temporal loss: ", temporal_loss(processed[i], rec))
             cnt_modulation += 1
             sf.write(SAVE_PATH / f"modulation_ref_{cnt_modulation}.wav", processed[i].T, 22050)
             sf.write(SAVE_PATH / f"modulation_rec_{cnt_modulation}.wav", rec.T, 22050)
         elif filename[i].split('-')[2][1:3] in ['21', '22'] and cnt_delay != 10:
             rec = model.board_layers[1].forward(clean[i], pred[i, 5:8])
+            rec = rec / torch.max(torch.abs(rec))
+            print('max processed', torch.max(torch.abs(processed[i])))
+            print('max rec', torch.max(torch.abs(rec)))
+            print("Spectral loss: ", spectral_loss(processed[i], rec))
+            print("temporal loss: ", temporal_loss(processed[i], rec))
             cnt_delay += 1
             sf.write(SAVE_PATH / f"delay_ref_{cnt_delay}.wav", processed[i].T, 22050)
             sf.write(SAVE_PATH / f"delay_rec_{cnt_delay}.wav", rec.T, 22050)
         elif filename[i].split('-')[2][0] == '4' and cnt_disto != 10:
             rec = model.board_layers[2].forward(clean[i], pred[i, 8:])
+            rec = rec / torch.max(torch.abs(rec))
+            print('max processed', torch.max(torch.abs(processed[i])))
+            print('max rec', torch.max(torch.abs(rec)))
+            print("Spectral loss: ", spectral_loss(processed[i], rec))
+            print("temporal loss: ", temporal_loss(processed[i], rec))
             cnt_disto += 1
             sf.write(SAVE_PATH / f"disto_ref_{cnt_disto}.wav", processed[i].T, 22050)
             sf.write(SAVE_PATH / f"disto_rec_{cnt_disto}.wav", rec.T, 22050)
