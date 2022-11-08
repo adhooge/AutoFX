ac4512eeafb6567199686fc967afd65c49237635
Alexandre <alexandre.dhooge@univ-lille.fr>
2022-11-08 12:28:57 +0100

    :wrench: helper script for datageneration
    probably not entirely finished, but it should work

--- data/datagen_mbfx.py
+++ data/datagen_mbfx.py
@@ -7,7 +7,6 @@ import warnings
 import numpy as np
 import torch
 
-from source.multiband_fx import MultiBandFX
 import source.util as util
 import random
 import pedalboard as pdb
@@ -19,43 +18,47 @@ from source.models.custom_distortion import CustomDistortion
 # warnings.filterwarnings("ignore", category=FutureWarning)
 def main(parser):
     args = vars(parser.parse_args())
-    DATA_PATH = pathlib.Path("/home/alexandre/dataset/guitar_mono_dry_22050_cut")
-    NUM_BANDS = 1
-    NUM_CHANGED_BANDS = 1
-    # FX = [pdb.Delay()]
-    FX = CustomDistortion()
-    PARAM_RANGE = [(0, 60),
-                   (50, 500), (-10, 10), (0.5, 2),
-                   (500, 2000), (-10, 10), (0.5, 2)]
-    PARAMS = ["p-drive_db",
-              "p-cutoff_frequency_hz_lo", "p-gain_db_lo", "p-q_lo",
-              "p-cutoff_frequency_hz_hi", "p-gain_db_hi", "p-q_hi"]
-    OUT_PATH = pathlib.Path("/home/alexandre/dataset/distortion_guitar_mono_cut2")
-
-    NUM_RUNS = 20
-
-    # TODO: Could automatically adapt to new FX
-
+    DATA_PATH = pathlib.Path(args['data-path'])
+    OUT_PATH = pathlib.Path(args['out-path'])
+    if not(args['modulation'] ^ args['distortion'] ^ args['delay']):
+        raise argparse.ArgumentError(None, message="One and only one effect must be selected.")
+    if args['modulation']:
+        FX = pdb.Chorus()
+        PARAM_RANGE = [(0.1, 10), (0, 1), (0, 20), (0, 1), (0, 1)]
+        PARAMS = ['p-rate_hz', 'p-depth', 'p-centre_delay_ms',
+                'p-feedback', 'p-mix']
+    elif args['delay']:
+        FX = pdb.Delay()
+        PARAM_RANGE = [(0, 1), (0, 1), (0, 1)]
+        PARAMS = ['p-delay', 'p-feedback', 'p-mix']
+    elif args['distortion']:
+        FX = CustomDistortion()
+        PARAM_RANGE = [(0, 60),
+                       (50, 500), (-10, 10), (0.5, 2),
+                       (500, 2000), (-10, 10), (0.5, 2)]
+        PARAMS = ["p-drive_db",
+                  "p-cutoff_frequency_hz_lo", "p-gain_db_lo", "p-q_lo",
+                  "p-cutoff_frequency_hz_hi", "p-gain_db_hi", "p-q_hi"]
+    else:
+        raise NotImplementedError("You should not be here.")
+    NUM_RUNS = args['num_runs']
     dataframe = pd.DataFrame(columns=PARAMS, dtype='float64')
-    # mbfx = MultiBandFX(FX, NUM_BANDS)
-    mbfx = FX
-    if not(OUT_PATH.exists()):
-        os.mkdir(OUT_PATH)
+    OUT_PATH.mkdir(parents=True, exist_ok=True)
     if (OUT_PATH / "params.csv").exists():
         raise ValueError("Output directory already has a params.csv file. Aborting.")
     for i in tqdm(range(NUM_RUNS), position=1):
         for file in tqdm(DATA_PATH.rglob('*.wav'), total=1872, position=0, leave=True):
+            # Generate random parameter values, rounded to the nearest hundredth
             params = [round(random.random(), 2) for p in range(len(PARAMS))]
-            # params.append(0)    #  freeze_mode for reverb
-            mbfx.set_fx_params(params, param_range=PARAM_RANGE, flat=True)
+            FX.set_fx_params(params, param_range=PARAM_RANGE, flat=True)
             audio, rate = util.read_audio(file, normalize=True, add_noise=False)
-            # processed = np.zeros((1, int(5.12*rate)))   # zero-pad to keep reverb tail (up to 5s)
-            # processed[0, :len(audio[0])] = audio
-            processed = mbfx.process(audio, rate)
+            processed = FX.process(audio, rate)
             dataframe.loc[file.stem + '_' + str(i)] = params
-            # audio = audio[0, ::2]
+            if args['downsample']:
+                audio = audio[0, ::2]
+                rate = rate // 2
             processed = processed[0] / torch.max(torch.abs(processed[0]))
-            sf.write(OUT_PATH / (file.stem + '_' + str(i) + file.suffix), processed, int(rate//1))      # CAREFUL WITH SUBSAMPLING
+            sf.write(OUT_PATH / (file.stem + '_' + str(i) + file.suffix), processed, int(rate))
         dataframe.to_csv(OUT_PATH / "params.csv")
         dataframe.to_pickle(OUT_PATH / "params.pkl")
     with open(OUT_PATH / "config.txt", 'w') as f:
@@ -64,17 +67,19 @@ def main(parser):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Script to process clean audio with randomly configured Fx")
+    parser = argparse.ArgumentParser(description="Script to process clean audio with randomly configured Fx.")
     parser.add_argument('--data-path', '-d', type=str,
-                        help="Path to the clean sounds")
+                        help="Path to the clean sounds.")
     parser.add_argument('--out-path', '-o', type=str,
-                        help="Path to store the produced files")
+                        help="Path to store the produced files.")
     parser.add_argument('--downsample', '-D', action='store_true',
-                        help="Flag to divide the sampling rate by 2")
+                        help="Flag to divide the sampling rate by 2.")
     parser.add_argument('--modulation', action='store_true',
-                        help="Use the Modulation effect")
+                        help="Use the Modulation effect.")
     parser.add_argument('--delay', action='store_true',
-                        help="Use the Delay effect")
+                        help="Use the Delay effect.")
     parser.add_argument('--distortion', action='store_true',
-                        help="Use Distortion effect")
+                        help="Use the Distortion effect.")
+    parser.add_argument('--num-runs', '-n', type=int, default=20,
+                        help="Number of runs over the clean sounds. Default is 20.")
     sys.exit(main(parser))
